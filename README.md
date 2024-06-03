da0bd272b2fe4342483241a6b9d35aa543eb6721
Ermak Anna <ermak-anna86@yandex.ru>
2024-06-03 15:55:20 +0300

    add readme

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Инструкция по запуску:
 
-- склонируйте репозиторий `git clone `
+- склонируйте репозиторий `git clone https://github.com/AnnAErmak/task-list.git`
 - установите зависимости `yarn install`
 - запустите `yarn dev`
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 import tsconfig from './tsconfig.json';
 
@@ -20,7 +20,8 @@ const parseTsConfigPaths = (paths: Record<string, string[]>): Record<string, str
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/task-list/',
   resolve: {
     alias: parseTsConfigPaths(tsconfig.compilerOptions.paths),
   },
-})
+});
